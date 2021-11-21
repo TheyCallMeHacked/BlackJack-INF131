58e460e43a1fa563019991ddc2a424f211827239
Alexandre Klein <alexandre.klein04@protonmail.com>
2021-11-21 16:47:46 +0100

    Finished B2 and B3, some corrections to be done about the dictionary accesses

--- players.py
+++ players.py
@@ -1,31 +1,31 @@
 # A2 - Players and scores
 
-import deck
+import deck as d
 
 # The players'scores are already given by initPlayer function so defining a "initScores" \
 # function is not necessary 
 
-def initPlayers(n, v = 0):
+def initPlayers(n, bal = 0):
     players = {}
     for i in range(n):
         name = input("Name of player " + str(i + 1) + ':')
         players[name] = {
             'score'        : [],
             'wins'         : 0,
-            'balance'      : v,
+            'balance'      : bal,
             'stillPlaying' : True,
             'bet'          : [0]
         }
     return players
 
 # By default the players' scores are initialized as null by "initPlayers" function
-def firstTurn(players, d):
-    for p in players:
-        s = deck.valueCard(deck.drawCard(d))
+def firstTurn(players, deck):
+    for player in players:
+        s = d.valueCard(d.drawCard(deck))
         s = 11 if s == 1 else s
-        t = deck.valueCard(deck.drawCard(d))
+        t = d.valueCard(d.drawCard(deck))
         t = 11 if s > 10 and t == 1 else s
-        p.score[0] = s + t
+        player.score[0] = s + t
 
 def winner(players):
     maxScore = 0
