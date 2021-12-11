d4c0c4c962b164b11b36782235bfa7a7b561a872
SP-84 <salloum.guillaume@gmail.com>
2021-12-11 15:22:11 +0100

    Added bots players with various strategies, updated the UI with Human / Bot flag

--- game_management.py
+++ game_management.py
@@ -1,22 +1,48 @@
 import deck as d
 import players as p
 import croupier as c
+import random
 
-def playerAction(bet, canDouble): # Renamed continue() to playerAction() because continue is a key-word. Also added 
-    prompt = "│Your action [hit, stand" + (", double down" if canDouble else "") + "]: "
-    noDoubleString = "│Your balance is too low to double down."
-    action = input(f"{' ':{' '}<80s}" + "\b│\r" + prompt).lower()
-    if action in ["double", "double down", "d"] and not canDouble:
-        print(f"{noDoubleString:{' '}<80s}" + "\b│")
-        action = ""
-    while action not in ["hit", "h",                    # Hit = Croupier draws a new card for that player
-                         "stand", "s",                  # Stand = The player takes no more cards
-                         "double", "double down", "d"]: # Double Down = The player's bet is doubled and the croupier draws an other card for that player
+def playerAction(player, bet, canDouble): # Renamed continue() to playerAction() because continue is a key-word.
+    if player['isBot'] == False:          # Human player action
+        prompt = "│Your action [hit, stand" + (", double down" if canDouble else "") + "]: "
+        noDoubleString = "│Your balance is too low to double down."
         action = input(f"{' ':{' '}<80s}" + "\b│\r" + prompt).lower()
         if action in ["double", "double down", "d"] and not canDouble:
             print(f"{noDoubleString:{' '}<80s}" + "\b│")
             action = ""
-    
+        while action not in ["hit", "h",                   # Hit = Croupier draws a new card for that player
+                            "stand", "s",                  # Stand = The player takes no more cards
+                            "double", "double down", "d"]: # Double Down = The player's bet is doubled and the croupier draws an other card for that player
+            action = input(f"{' ':{' '}<80s}" + "\b│\r" + prompt).lower()
+            if action in ["double", "double down", "d"] and not canDouble:
+                print(f"{noDoubleString:{' '}<80s}" + "\b│")
+                action = ""
+    else:                                  # Bot player action
+        if player['strategy'] == 0:
+            action = random.randint(0, 2)  # Totally random strategy : no risk aversion
+            if action == 0:                # 0 is stand
+                return False
+            elif action == 1:              # 1 is double down
+                bet[0] *2
+            else:
+                return True                # default is hit
+
+        elif 0 < player['strategy'] <= 50: # Low risk strategy : hit if score is less than 13 else stand
+            if player['score'][-1] < 13:
+                return True
+            else:
+                return False 
+
+        elif 50 < player['strategy'] <= 75: # Measured risk strategy : hit if score is less than 16 else stand
+            if player['score'][-1] < 16:
+                return True
+            else:
+                return False
+
+        else:                              # High risk strategy : hit against all odds
+            return True                   
+
     if action in ["stand", "s"]:
         return False
     if action in ["double", "double down", "d"]:
@@ -30,10 +56,14 @@ def playerAction(bet, canDouble): # Renamed continue() to playerAction() because
 # deck: the deck of cards
 def playerTurn(player, playerName, turnNumber, deck, nPlayers):
     score = player["score"][-1]
-    form = f"┤ Round {turnNumber} ; Player: {playerName} ; Score: {score} ├"
+    botString = " ; Bot"
+    humanString = " ; Human"
+    form = f"┤ Round {turnNumber} ; Player: {playerName}{botString if player['isBot'] == True else humanString} ; Score: {score} ├"
     print(f"{form:{'─'}^80s}" + "\b┤\r├")
     canDouble = (player["bet"][0]*2) <= player["balance"]
-    con = playerAction(player["bet"], canDouble)
+    con = playerAction(player, player["bet"], canDouble)
+    botActionString = f"|" + playerName + (" hits" if con == True else " stands") + "."
+    print(f"{botActionString:{' '}<80s}" + "\b│") 
     if con:
         c = d.drawCard(deck, nPlayers)[0]
         drawText = f"│{playerName}'s draw: {c}"
@@ -68,25 +98,26 @@ def completeGame(players, deck):
         player["score"].append(0)
         player["stillPlaying"] = True
         sanitised = False
-        while not sanitised:
-            bal = player["balance"]
-            bet = input(f"{name}'s bet (balance: ${bal:.2f}): ").split('.')     # splitting the input in integer and decimal part
-            if len(bet) > 2 or len(bet) < 0:                                    # if there are more than two elements in the list, the input was not a valid real number
-                print("Invalid value")
-            else:                                                               # otherwise, the input sanitisation can continue
-                flag = True                                                     # test if all the inputed characters (excluding the decimal point) were digits
-                for i in ''.join(bet):
-                    if i not in "0123456789":
-                        flag = False
-                if flag:                                                        # if it's the case, the input is finally sanitised and can be processed
-                    bet = float('.'.join(bet))
-                    if bet > bal or bet < 5:                                    # cannot bet more than you own or less than $5
-                        print("Invalid value")
-                    else:
-                        player["bet"] = [bet]
-                        sanitised = True
-                else:                                                           # otherwise the input was not a valid number
+        if player['isBot'] == False:                                                    # Manual bets apply to humans only
+            while not sanitised:
+                bal = player["balance"]
+                bet = input(f"{name}'s bet (balance: ${bal:.2f}): ").split('.')     # splitting the input in integer and decimal part
+                if len(bet) > 2 or len(bet) < 0:                                    # if there are more than two elements in the list, the input was not a valid real number
                     print("Invalid value")
+                else:                                                               # otherwise, the input sanitisation can continue
+                    flag = True                                                     # test if all the inputed characters (excluding the decimal point) were digits
+                    for i in ''.join(bet):
+                        if i not in "0123456789":
+                            flag = False
+                        if flag:                                                        # if it's the case, the input is finally sanitised and can be processed
+                            bet = float('.'.join(bet))
+                            if bet > bal or bet < 5:                                    # cannot bet more than you own or less than $5
+                                print("Invalid value")
+                            else:
+                                player["bet"] = [bet]
+                                sanitised = True
+                        else:                                                           # otherwise the input was not a valid number
+                            print("Invalid value")
 
     blackJack = p.firstTurn(players, deck)
     if blackJack:
--- players.py
+++ players.py
@@ -15,21 +15,41 @@ def initPlayers(n, bal = 100):
             'balance'      : bal,
             'stillPlaying' : True,
             'bet'          : [0],
-            'hand'         : []
+            'hand'         : [],
+            'isBot'        : False
         }
+        isBot = input("Is " + name + " a human or a bot [H/b]? ").lower() in ["b", "bot"]
+        players[name]['isBot'] = isBot
+        if players[name]['isBot'] == True:
+            playfullness = False
+            while type(playfullness) != float:
+                try:
+                    inp = float(input("Enter the bot risk-averse scale (enter a percentage between 0 and 100) : "))
+                except ValueError:
+                    print("The value should be a real number between 0 and 100")
+                else :
+                    if 0 <= inp <= 100:
+                        playfullness = inp
+                    else:
+                        print("The value should be a real number between 0 and 100")
+                    
+            players[name]['strategy'] = playfullness
+
     return players
 
 # By default the players' scores are initialized as null by "initPlayers" function
 def firstTurn(players, deck):
     flag = False
     for i, (name, player) in enumerate(players.items()):
-        form = f"┤ Round 1 ; Player: {name} ├"
+        botString = " ; Bot"
+        humanString = " ; Human"
+        form = f"┤ Round 1 ; Player: {name}{botString if players[name]['isBot'] == True else humanString} ├"
         if i == 0:
             print(f"{form:{'─'}^80s}" + "\b╮\r╭")
         else:
-            print(f"{form:{'─'}^80s}" + "\b┤\r├")
+            print(f"{form:{'─'}^80s}" + "\b┤\r├")                                   
         c = d.drawCard(deck, len(players), 2)
-        drawText = f"│{name}'s fist draw: {c[0]} {c[1]}"
+        drawText = f"│{name}'s first draw: {c[0]} {c[1]}"
         print(f"{drawText:<80s}" + "\b│")
         player["hand"] = c
         player["score"][-1] = d.calcScore(player["hand"])
